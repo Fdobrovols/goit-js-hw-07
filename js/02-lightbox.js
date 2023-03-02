42b554f945c2298d666557d1958d38a6f2a9198b
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-03-02 22:25:07 +0200

    FullHWJS7

---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+//REFERENCES
+const galleryContainerRef = document.querySelector('.gallery');
+
+//Creating markup
+galleryContainerRef.insertAdjacentHTML('afterbegin', creatingMarkup(galleryItems));
+
+function creatingMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => 
+    {
+      return `<li><a href="${original}" class="gallery__link">
+      <img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`;
+    }).join('');
+}
+let gallery = new SimpleLightbox('.gallery a',
+{
+   captionData: 'alt',
+   captionDelay: 250,
+   scrollZoomFactor: 0.1,
+});
